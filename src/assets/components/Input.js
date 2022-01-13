28b3ee1ee9daa45f5e5bbc570e62bc8888ad0d4a
Lucas <lucasazzollinivieira@gmail.com>
2022-01-12 23:36:26 -0300

    generic input done

---
+++ src/assets/components/Input.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+    width: 100%;
+    height: 70px; 
+
+    font-size: 20px;
+    font-weight: 600;
+    
+    padding-left: 35px;
+    margin-bottom: 25px;
+
+    border-radius: 7px;
+    border: 2px solid #E9E9E9;
+
+    ::placeholder {        
+        font-size: 20px;
+        font-weight: 600;
+        
+        color: #6B7280;
+    }
+
+`
+
+export default Input;
