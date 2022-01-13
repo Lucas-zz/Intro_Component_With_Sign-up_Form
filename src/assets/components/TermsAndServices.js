7430667df9dcd84f966d1714ad6c0c5bd0bbcba3
Lucas <lucasazzollinivieira@gmail.com>
2022-01-12 23:37:02 -0300

    terms and services text done

---
+++ src/assets/components/TermsAndServices.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const TermsAndServices = styled.div`
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 15px;
+    font-weight: 600;
+
+    padding-top: 25px;
+
+    color: lightgrey;
+
+    mark {
+        all: unset;
+        color: #FF7A7B;
+        padding-left: 5px;
+
+        cursor: pointer;
+    }
+`
+
+export default TermsAndServices;
